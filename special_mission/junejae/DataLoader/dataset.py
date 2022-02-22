f081de7487bbaec8b24adf2f406a5c8fce0995b5
Junejae <mykjj1029@Knights.ucf.edu>
2022-02-22 13:22:28 +0000

    init

--- special_mission/junejae/DataLoader/dataset.py
+++ special_mission/junejae/DataLoader/dataset.py
@@ -1,26 +1,31 @@
+from preprocess import preprocessFunction
 from torch.utils.data.dataset import dataset
 from torchvision import transforms
 import pandas as pd
 import numpy as np
 
+
 class MyCustomDataset(Dataset):
-    def __init__(self, csv_path):
+    def __init__(self, path):
+        # assuming path = '/opt/ml/input/data/train'
+        self.path = path
+        self.data_info = preprocessFunction()
 
         self.to_tensor = transforms.ToTensor()
 
-        self.data_info = pd.read_csv(csv_path, header=None)
+        self.image_arr = np.asarray(self.data_info.img_path)
+        self.label_arr = np.asarray(self.data_info.label)
 
-        self.image_arr = np.asarray(self.data_info.iloc[:, 0])
+        self.data_len = len(self.data_info.id)
 
-        self.label_arr = 
-    
     def __getitem__(self, index):
+        img_path = path + "/images/" + self.image_arr[index]
+        img_thing = Image.open(img_path)
 
+        img_tensor = self.to_tensor(img_thing)
+        img_label = self.label_arr[index]
 
-        return (img, label)
+        return (img_tensor, img_label)
 
     def __len__(self):
-
-        return count
-
-if __name__ == '__main__':
+        return self.data_len
