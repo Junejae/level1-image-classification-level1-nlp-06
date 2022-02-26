c831f57ce8ce5ce8da1f33af6f8a043254e7daac
HyunAhKim <wgrape0@gmail.com>
2022-02-26 21:56:29 +0900

    reset dataset.py && EDA-ColorJitter-saturation

--- special_mission/hyunah/EDA.py
+++ special_mission/hyunah/EDA.py
@@ -1,15 +1,19 @@
 import matplotlib.pyplot as plt
 from torchvision.transforms.functional import to_pil_image # tensor to pil_image
 import torch
+from dataset import MaskLabels, GenderLabels, AgeLabels
 
 def show_images(dataset, idxs, rows=1, cols=7):
     plt.figure(figsize=(20,14))
-
+    
     for i, idx in enumerate(idxs):
         plt.subplot(rows, cols, i+1)
-        img, label = dataset[idx]
-        plt.imshow(to_pil_image(img))
-        plt.title(label, fontsize = 16)
+        
+        img = dataset.read_image(idx)        
+        _, label = dataset[idx]
+        
+        plt.imshow(img)
+        plt.title(dataset.decode_multi_class(label), fontsize = 16)
         plt.axis('off')
     plt.show()
 
